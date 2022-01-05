abc5692b2dd84f30f55005e55c3cca60d4c9107c
mrPsycox <silvestri,1699926@studenti.uniroma1.it>
2022-01-05 19:51:37 +0100

    fix

--- testingcli.py
+++ testingcli.py
@@ -3,8 +3,7 @@ import unittest
 
 class first_lines_testclass(unittest.TestCase):
     def test_lastlines_emptytext(self):
-        self.assertEqual(last_lines(["ciaone\n","pisellone\n","amostro\n"],1), "amostro")
+        self.assertEqual(last_lines(["hi\n","there\n","wow\n"],1), "wow")
 
 if __name__ == '__main__':
     unittest.main()
-    #print(last_lines(["ciaone\n","pisellone\n","amostro\n"],1))
